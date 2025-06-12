398b6b94e10a9ae724db52200a89e8829c3627e7
datlt2306 <datlt2306@gmail.com>
2025-06-12 17:37:32 +0700

    feat(product): thêm chức năng xóa sản phẩm và hiển thị chi tiết sản phẩm
    - Cập nhật hàm `fetchResource` để hỗ trợ tùy chọn phương thức.
    - Thêm hàm `deleteProduct` để xóa sản phẩm và làm mới trang sau khi xóa.
    - Cải thiện giao diện hiển thị sản phẩm với liên kết đến trang chi tiết sản phẩm.
    - Thêm hàm `renderProductDetail` để hiển thị thông tin chi tiết sản phẩm khi có ID trong URL.

--- ontap/fetch/main.js
+++ ontap/fetch/main.js
@@ -1,9 +1,15 @@
 const API_URL = "https://api.fake-rest.refine.dev";
 
-async function fetchResource(url) {
-    const response = await fetch(url);
+async function fetchResource(url, options) {
+    const response = await fetch(url, options);
     if (!response.ok) throw new Error("Lỗi khi lấy dữ liệu!");
     return response.json();
+}
+
+async function deleteProduct(id) {
+    await fetchResource(`${API_URL}/products/${id}`, { method: "DELETE" });
+    alert("Xóa sản phẩm thành công!");
+    window.location.reload();
 }
 function renderProducts(products) {
     const productListEl = document.querySelector("#product-list");
@@ -16,13 +22,36 @@ function renderProducts(products) {
                     >
                         ${index + 1}
                     </div>
-                    <span class="text-gray-800 font-medium">${product.name}</span>
+                    <span class="text-gray-800 font-medium">
+                    <a href="detail.html?id=${product.id}">${product.name}</a>
+                    </span>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md" onclick="deleteProduct(${
+                        product.id
+                    })">Xóa</button>
                 </li>`;
         })
         .join("");
 }
 
+function renderProductDetail(product) {
+    const productDetailEl = document.querySelector("#product-detail");
+    if (productDetailEl) {
+        productDetailEl.innerHTML = `
+            <h1>${product.name}</h1>
+            <p>${product.description}</p>
+            <p>${product.price}</p>
+        `;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const products = await fetchResource(`${API_URL}/products?_limit=15`);
     renderProducts(products);
+
+    // Chi tiết sản phẩm
+    const id = new URLSearchParams(window.location.search).get("id");
+    if (id) {
+        const product = await fetchResource(`${API_URL}/products/${id}`);
+        renderProductDetail(product);
+    }
 });
